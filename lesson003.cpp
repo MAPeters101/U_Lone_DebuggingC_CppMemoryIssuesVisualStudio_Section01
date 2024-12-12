239bb135d8e79ade55bf2f9ce739912bd7a3a74a
Mark Peters <mapeters101@earthlink.net>
2024-12-12 14:33:32 -0500

    Work on lesson003.

---
+++ lesson003.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+int g_Value{42};
+
+int main() {
+	int value{101};
+	int* p1 = &value;
+	int* p2 = new int{5};
+
+	int* p3 = &g_Value;
+
+	int(*pFn)() = main;
+
+
+	cout << "Value of value: " << value << endl;
+	cout << "Address of value: " << &value << endl;
+
+	cout << "\nValue of p1: " << p1 << endl;
+	cout << "Address of p1: " << &p1 << endl;
+	cout << "Value pointed to by p1: " << *p1 << endl;
+
+	cout << "\nValue of p2: " << p2 << endl;
+	cout << "Address of p2: " << &p2 << endl;
+	cout << "Value pointed to by p2: " << *p2 << endl;
+
+	cout << "\nValue of g_Value: " << g_Value << endl;
+	cout << "Address of g_Value: " << &g_Value << endl;
+	cout << "Value of p3: " << p3 << endl;
+	cout << "Address of p3: " << &p3 << endl;
+	cout << "Value pointed to by p3: " << *p3 << endl;
+
+	cout << "\nAddress of main: " << &main << endl;
+	cout << "Value of pFn: " << pFn << endl;
+	cout << "Address of pFn: " << &pFn << endl;
+	cout << "Value pointed to by pFn: " << *pFn << endl;
+
+	return 0;
+}
+
+*/
