8488bc4aa36d3a514bbe13a3b587a00b892ba6f7
Mark Peters <mapeters101@earthlink.net>
2024-12-13 14:17:55 -0500

    Work on lesson009.

---
+++ lesson009.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int* ptr = static_cast<int*>(calloc(1, sizeof(int)));
+	*ptr = 7;
+	cout << *ptr << endl;
+	cout << "Initial mem size: " << _msize(ptr) << endl;
+	//ptr = static_cast<int*>(realloc(ptr, sizeof(int) * UINT32_MAX));
+	if (ptr == NULL) {
+		cout << "Could not allocate memory" << endl;
+		return -1;
+	}
+	cout << "After mem size: " << _msize(ptr) << endl;
+
+	free(ptr);
+
+	return 0;
+}
+*/
