b3fbc01bcff7b70431d01017e38cd8549b8babd4
Mark Peters <mapeters101@earthlink.net>
2024-12-13 11:29:51 -0500

    Work on lesson008.

---
+++ lesson008.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+	//int* ptr = static_cast<int*>(malloc(sizeof(int)));
+	int* ptr = static_cast<int*>(calloc(1, sizeof(int)));
+	*ptr = 7;
+	cout << *ptr << endl;
+	free(ptr);
+
+
+ 	return 0;
+ }
+ */
+
